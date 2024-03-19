063c0ffaf0afcb2165ba597051a7696f92459b4a
nahid <nahidswe@gmail.com>
2024-03-19 21:13:51 +0600

    chal dal demo added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import RecipeReviewCard from './components/card/RecipeReviewCard';
+import CardGpt from './components/card/CardGpt';
+import ProductCard from './components/card/ProductCard';
+
 
 function App() {
+  const product = {
+    name : "Nahid",
+    description : "I am nahid Hasan Lovon",
+    image: "https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1-1024x824.jpg"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <RecipeReviewCard />
+      <CardGpt product={product}/> */}
+      <ProductCard />
     </div>
   );
 }
---
+++ src/components/card/ProductCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import '../../style/ProductCardStyle.scss'
+const cardContainer =  {
+    height: "300px",
+    width: "200px",
+    boxShadow: "3px 3px 16px 7px rgba(128, 122, 124, 0.35)"
+}
+function ProductCard() {
+  return (
+    <>
+      <div style={cardContainer} >
+hiiiiiiiiiii
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
