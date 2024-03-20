9dada887baef8ea171162b442bfbbead5b87b642
nahidswe <nahidswe@gmail.com>
2024-03-21 01:32:29 +0600

    sidebar data added partially.

---
+++ src/components/sidebar/SidebarData.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const category = [
+    {
+        name: "Popular",
+        subCategory: []
+    },
+    {
+        name: "Food",
+        subCategory: [
+            {
+                name: "Fresh Vegetables",
+                subCategory: []
+            },
+            {
+                name: "Fresh Fruits",
+                subCategory: []
+            },
+            {
+                name: "Dates (Khejur)",
+                subCategory: []
+            }
+        ]
+    }
+
+]
