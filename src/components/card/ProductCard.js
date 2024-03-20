aef9cf4c5254cbb74f1d93c2d50914f2d3ae3687
nahidswe <nahidswe@gmail.com>
2024-03-20 11:14:15 +0600

    product card initiated.

--- src/components/card/ProductCard.js
+++ src/components/card/ProductCard.js
@@ -1,15 +1,64 @@
 import React from "react";
-// import '../../style/ProductCardStyle.scss'
-const cardContainer =  {
-    height: "300px",
-    width: "200px",
-    boxShadow: "3px 3px 16px 7px rgba(128, 122, 124, 0.35)"
+import '../../style/ProductCardStyle.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card, CardActionArea, CardContent, CardMedia, Typography, Button } from '@mui/material';
+
+
+const cardContainer = {
+  height: "300px",
+  width: "200px",
+  boxShadow: "3px 3px 16px 7px rgba(128, 122, 124, 0.35)"
 }
 function ProductCard() {
+
+  const amount = 100899999;
+  const measurement = "10kg";
+  const productTitle = "Junior is a person who is smaller in age so what do you think is it write to"
   return (
     <>
-      <div style={cardContainer} >
-hiiiiiiiiiii
+    {/* <div className="test">
+      <div className="contents">Hello</div>
+      
+    </div> */}
+    
+      <div className="card-container">
+        <div className="card-background">
+          <div className="product-image-wrapper">
+            <CardMedia
+              component="img"
+              height="154"
+              image="https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1-1024x824.jpg"
+              alt="prodcut"
+              className="product-image"
+            />
+          </div>
+          <div className="product-title-wrapper">
+
+          </div>
+          <div className="product-title">
+            {productTitle}
+          </div>
+          <div className="measurement-wrapper">
+            <div className="measurement">
+              {measurement}
+            </div>
+          </div>
+          <div className="price-container-wrapper">
+            <div className="price-container">
+              <FontAwesomeIcon className="dollar-icon" icon="fa-solid fa-bangladeshi-taka-sign" />
+              <span className="amount">
+                {amount}
+              </span>
+            </div>
+          </div>
+          <div className="card-footer">
+          </div>
+        </div>
+
+        
+          <div className="card-forground" >hjhjkjhkjhkjhkj
+          </div>
+        
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Library, library } from '@fortawesome/fontawesome-svg-core';
+import { faS, fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
